cf2fb03d3f1f64580b52980c35fd8fe47b86e884
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-02-03 16:16:23 +0100

    [#1786] Added a default implementation of the FacetedSearchParameters

---
+++ src/main/java/nl/knaw/huygens/facetedsearch/model/DefaulFacetedSearchParameters.java
@@ -0,0 +1,8 @@
+package nl.knaw.huygens.facetedsearch.model;
+
+/**
+ * Default implementation of {@code FacetedSearchParameters}.
+ */
+public class DefaulFacetedSearchParameters extends FacetedSearchParameters<DefaulFacetedSearchParameters> {
+
+}
