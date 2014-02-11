a90507f818fad3f46f7ecc8bd7568c50a5a47abd
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-02-11 10:43:27 +0100

    [#1786] Code clean up.

--- src/main/java/nl/knaw/huygens/facetedsearch/model/NoSuchFieldInIndexException.java
+++ src/main/java/nl/knaw/huygens/facetedsearch/model/NoSuchFieldInIndexException.java
@@ -4,8 +4,8 @@ public class NoSuchFieldInIndexException extends Exception {
 
   private static final long serialVersionUID = 1L;
 
-  public NoSuchFieldInIndexException(String facetName) {
-    super(String.format("Field with name \"%s\" does not exist", facetName));
+  public NoSuchFieldInIndexException(String fieldName) {
+    super(String.format("Field with name \"%s\" does not exist", fieldName));
   }
 
 }
