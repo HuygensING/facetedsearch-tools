3d3427cbbdf71f3294d8e0009b3bd09c27e397aa
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-02-20 09:57:18 +0100

    [#1786] Tests for FacetedSearchLib added when the QueryCreator throws exceptions

--- src/test/java/nl/knaw/huygens/solr/FacetedSearchLibraryTest.java
+++ src/test/java/nl/knaw/huygens/solr/FacetedSearchLibraryTest.java
@@ -3,7 +3,10 @@ package nl.knaw.huygens.solr;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import nl.knaw.huygens.facetedsearch.model.DefaultFacetedSearchParameters;
 import nl.knaw.huygens.facetedsearch.model.NoSuchFieldInIndexException;
@@ -27,6 +30,8 @@ public class FacetedSearchLibraryTest {
   private QueryResponse queryResponseMock;
   private SearchResult searchResultMock;
 
+  private DefaultFacetedSearchParameters searchParameters;
+
   @Before
   public void setUp() {
     queryCreatorMock = mock(SolrQueryCreator.class);
@@ -37,28 +42,54 @@ public class FacetedSearchLibraryTest {
     searchResultMock = mock(SearchResult.class);
 
     instance = new FacetedSearchLibrary(solrCoreMock, queryCreatorMock);
-
     instance.setSearchResultBuilder(searchResultBuilderMock);
+
+    searchParameters = new DefaultFacetedSearchParameters();
   }
 
   @Test
   public void testSearch() throws NoSuchFieldInIndexException, WrongFacetValueException {
-    DefaultFacetedSearchParameters parameters = new DefaultFacetedSearchParameters();
-
     // when
     when(queryCreatorMock.createSearchQuery(any(DefaultFacetedSearchParameters.class), any(FacetedSearchParametersValidator.class))).thenReturn(queryMock);
     when(solrCoreMock.query(queryMock)).thenReturn(queryResponseMock);
     when(searchResultBuilderMock.build(queryResponseMock)).thenReturn(searchResultMock);
 
     // action
-    SearchResult result = instance.search(parameters);
+    SearchResult result = instance.search(searchParameters);
 
     // verify
     InOrder inOrder = Mockito.inOrder(queryCreatorMock, solrCoreMock, searchResultBuilderMock);
-    inOrder.verify(queryCreatorMock).createSearchQuery(parameters, null);
+    inOrder.verify(queryCreatorMock).createSearchQuery(searchParameters, null);
     inOrder.verify(solrCoreMock).query(queryMock);
     inOrder.verify(searchResultBuilderMock).build(queryResponseMock);
     assertThat(result, is(searchResultMock));
+  }
+
+  @Test(expected = NoSuchFieldInIndexException.class)
+  public void TestSearchQueryCreatorThrowsNoSuchFieldInIndexException() throws NoSuchFieldInIndexException, WrongFacetValueException {
+
+    testSearchQueryCreatorThrowsException(NoSuchFieldInIndexException.class);
+
+  }
+
+  @Test(expected = WrongFacetValueException.class)
+  public void TestSearchQueryCreatorThrowsWrongFacetValueException() throws NoSuchFieldInIndexException, WrongFacetValueException {
+    testSearchQueryCreatorThrowsException(WrongFacetValueException.class);
+  }
+
+  private void testSearchQueryCreatorThrowsException(Class<? extends Exception> exception) throws NoSuchFieldInIndexException, WrongFacetValueException {
+    // when
+    doThrow(exception).when(queryCreatorMock).createSearchQuery(any(DefaultFacetedSearchParameters.class), any(FacetedSearchParametersValidator.class));
+    // action
+    try {
+      instance.search(searchParameters);
+    } finally {
+
+      // verify
+      verify(queryCreatorMock).createSearchQuery(searchParameters, null);
+      verify(solrCoreMock, never()).query(any(SolrQuery.class));
+      verify(searchResultBuilderMock, never()).build(any(QueryResponse.class));
+    }
   }
 
 }
