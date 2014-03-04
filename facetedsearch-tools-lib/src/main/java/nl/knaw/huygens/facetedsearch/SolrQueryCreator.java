3d14d004c58d7d5b78dc2816953bffbb7e0514c2
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-03-04 10:23:25 +0100

    [#1786] Restructured the project in multiple modules

---
+++ facetedsearch-tools-lib/src/main/java/nl/knaw/huygens/facetedsearch/SolrQueryCreator.java
@@ -0,0 +1,134 @@
+package nl.knaw.huygens.facetedsearch;
+
+import java.util.List;
+
+import nl.knaw.huygens.facetedsearch.model.NoSuchFieldInIndexException;
+import nl.knaw.huygens.facetedsearch.model.parameters.FacetParameter;
+import nl.knaw.huygens.facetedsearch.model.parameters.FacetedSearchParameters;
+import nl.knaw.huygens.facetedsearch.model.parameters.HighlightingOptions;
+import nl.knaw.huygens.facetedsearch.model.parameters.QueryOptimizer;
+import nl.knaw.huygens.facetedsearch.model.parameters.SortParameter;
+import nl.knaw.huygens.facetedsearch.services.SolrUtils;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrQuery.ORDER;
+import org.apache.solr.client.solrj.SolrQuery.SortClause;
+import org.apache.solr.common.params.HighlightParams;
+
+public class SolrQueryCreator {
+
+  public <T extends FacetedSearchParameters<T>> SolrQuery createSearchQuery(FacetedSearchParameters<T> searchParameters) {
+    SolrQuery query = new SolrQuery();
+    query.setQuery(createQuery(searchParameters));
+    query.setFields(getResultFields(searchParameters));
+    query.addFacetField(getFacetFields(searchParameters));
+    query = setSort(query, searchParameters);
+    query = setHighlighting(query, searchParameters);
+
+    optimizeQuery(query, searchParameters);
+
+    return query;
+  }
+
+  private <T extends FacetedSearchParameters<T>> SolrQuery optimizeQuery(SolrQuery query, FacetedSearchParameters<T> searchParameters) {
+    QueryOptimizer optimizer = searchParameters.getQueryOptimizer();
+
+    if (optimizer != null) {
+      query.setRows(optimizer.getRows());
+      query.setFacetLimit(optimizer.getFacetLimit());
+      query.setFacetMinCount(optimizer.getFacetMinCount());
+    }
+
+    return query;
+  }
+
+  private <T extends FacetedSearchParameters<T>> SolrQuery setHighlighting(SolrQuery query, FacetedSearchParameters<T> searchParameters) {
+    HighlightingOptions highlightingOptions = searchParameters.getHighlightingOptions();
+
+    if (highlightingOptions != null && searchParameters.getFullTextSearchFields() != null) {
+      query.setHighlight(true);
+      query.setHighlightFragsize(highlightingOptions.getFragSize());
+      query.set(HighlightParams.MAX_CHARS, highlightingOptions.getMaxChars());
+      query.set(HighlightParams.MERGE_CONTIGUOUS_FRAGMENTS, highlightingOptions.isMergeContiguous());
+      query.set(HighlightParams.FIELDS, searchParameters.getFullTextSearchFields().toArray(new String[0]));
+    }
+
+    return query;
+  }
+
+  private <T extends FacetedSearchParameters<T>> String[] getResultFields(FacetedSearchParameters<T> searchParameters) {
+
+    return searchParameters.getResultFields().toArray(new String[0]);
+  }
+
+  private <T extends FacetedSearchParameters<T>> String[] getFacetFields(FacetedSearchParameters<T> searchParameters) {
+
+    return searchParameters.getFacetFields().toArray(new String[0]);
+  }
+
+  private <T extends FacetedSearchParameters<T>> String createQuery(FacetedSearchParameters<T> searchParameters) {
+    StringBuilder builder = new StringBuilder();
+    List<String> fullTextSearchFields = searchParameters.getFullTextSearchFields();
+    List<FacetParameter> facetValues = searchParameters.getFacetValues();
+    boolean useFacets = facetValues != null && !facetValues.isEmpty();
+
+    String prefix = "";
+    String term = searchParameters.getTerm();
+    if (fullTextSearchFields != null && !isWildCardQuery(term)) {
+      for (String field : fullTextSearchFields) {
+        builder.append(prefix).append(useFacets ? "+" : "").append(field).append(":");
+        builder.append(formatTerm(term, searchParameters.isFuzzy()));
+        prefix = " "; // separate the search field searches
+      }
+    } else if (!useFacets) {
+      builder.append("*:*");
+    }
+
+    if (useFacets) {
+      for (FacetParameter facetParameter : facetValues) {
+        String name = facetParameter.getName();
+
+        builder.append(" +").append(name).append(":");
+        builder.append(facetParameter.getQueryValue());
+      }
+    }
+
+    return builder.toString();
+  }
+
+  private boolean isWildCardQuery(String term) {
+    return StringUtils.isBlank(term) || term.equals("*") || term.equals("*:*");
+  }
+
+  private String formatTerm(String term, boolean fuzzy) {
+    if (fuzzy) {
+      term = SolrUtils.fuzzy(term);
+    }
+
+    if (term.trim().contains(" ")) {
+      return String.format("(%s)", term);
+    }
+    return term;
+  }
+
+  /**
+   * Sets the sort criteria for the query.
+   * @param query the {@code SolrQuery} that should be sorted.
+   * @param searchParameters that container of the {@code SortParameter}s.
+   * @return the query, that went in plus the added {@code SortClause}s.
+   * @throws NoSuchFieldInIndexException if one of the {@code SortParameter}s does not exist.
+   */
+  private <T extends FacetedSearchParameters<T>> SolrQuery setSort(SolrQuery query, FacetedSearchParameters<T> searchParameters) {
+    List<SortParameter> sortParameters = searchParameters.getSortParameters();
+    for (SortParameter sortParameter : sortParameters) {
+      query.addSort(createSortClause(sortParameter));
+    }
+    return query;
+  }
+
+  private SortClause createSortClause(SortParameter sortParameter) {
+    ORDER order = ORDER.valueOf(sortParameter.getDirection().toString());
+    return new SortClause(sortParameter.getFieldname(), order);
+  }
+}
---
+++ facetedsearch-tools-model/src/main/java/nl/knaw/huygens/facetedsearch/model/parameters/DefaultFacetParameter.java
@@ -0,0 +1,81 @@
+package nl.knaw.huygens.facetedsearch.model.parameters;
+
+import java.util.List;
+
+import nl.knaw.huygens.facetedsearch.services.SolrUtils;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
+import com.google.common.collect.Lists;
+
+public class DefaultFacetParameter extends FacetParameter {
+  // TODO: make it work with jackson/jersey
+  private List<String> values = Lists.newArrayList();
+
+  public DefaultFacetParameter(String name, List<String> values) {
+    super(name);
+    this.values = values;
+  }
+
+  public List<String> getValues() {
+    return values;
+  }
+
+  public DefaultFacetParameter setValues(List<String> values) {
+    this.values = values;
+    return this;
+  }
+
+  public List<String> getEscapedValues() {
+    Builder<String> builder = ImmutableList.builder();
+    for (String value : getValues()) {
+      builder.add(SolrUtils.escapeFacetValue(value));
+    }
+    return builder.build();
+  }
+
+  private long lowerLimit = -1;
+  private long upperLimit = -1;
+
+  public DefaultFacetParameter setLowerLimit(long lowerLimit) {
+    this.lowerLimit = lowerLimit;
+    return this;
+  }
+
+  public long getLowerLimit() {
+    return lowerLimit;
+  }
+
+  public DefaultFacetParameter setUpperLimit(long upperLimit) {
+    this.upperLimit = upperLimit;
+    return this;
+  }
+
+  public long getUpperLimit() {
+    return upperLimit;
+  }
+
+  public boolean isRangeFacetParameter() {
+    return lowerLimit != -1 && upperLimit != -1;
+  }
+
+  @Override
+  public String getQueryValue() {
+    List<String> values = this.getValues();
+
+    if (values.size() > 1) {
+      StringBuilder builder = new StringBuilder();
+      builder.append("(");
+      String prefix = "";
+      for (String value : values) {
+        builder.append(prefix).append(SolrUtils.escapeFacetValue(value));
+        prefix = " ";
+      }
+      builder.append(")");
+      return builder.toString();
+    }
+    return SolrUtils.escapeFacetValue(values.get(0));
+
+  }
+
+}
