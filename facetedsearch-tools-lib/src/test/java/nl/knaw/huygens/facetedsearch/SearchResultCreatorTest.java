920d2197edbd75afac31f53ed0aadc4ecc0f7726
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-03-04 12:13:57 +0100

    [#1786] Code clean up

--- facetedsearch-tools-lib/src/test/java/nl/knaw/huygens/facetedsearch/SearchResultCreatorTest.java
+++ facetedsearch-tools-lib/src/test/java/nl/knaw/huygens/facetedsearch/SearchResultCreatorTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import nl.knaw.huygens.facetedsearch.SearchResultCreator;
 import nl.knaw.huygens.facetedsearch.converters.FacetListConverter;
 import nl.knaw.huygens.facetedsearch.converters.HighlightingConverter;
 import nl.knaw.huygens.facetedsearch.converters.ResultConverter;
@@ -13,10 +12,10 @@ import nl.knaw.huygens.facetedsearch.model.FacetedSearchResult;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.junit.Test;
 
-public class SearchResultBuilderTest {
+public class SearchResultCreatorTest {
 
   @Test
-  public void testSearchResultBuilder() {
+  public void testCreate() {
     //mock
     QueryResponse queryResponseMock = mock(QueryResponse.class);
     FacetListConverter facetConverterMock = mock(FacetListConverter.class);
