1e290cee63bbd86aaa8704223d7120ada8143148
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-06-23 15:54:11 +0200

    Code clean up

--- facetedsearch-tools-definition/src/main/java/nl/knaw/huygens/facetedsearch/definition/SolrSearcher.java
+++ facetedsearch-tools-definition/src/main/java/nl/knaw/huygens/facetedsearch/definition/SolrSearcher.java
@@ -1,9 +1,5 @@
 package nl.knaw.huygens.facetedsearch.definition;
 
-import java.util.List;
-import java.util.Map;
-
-import nl.knaw.huygens.facetedsearch.model.FacetDefinition;
 import nl.knaw.huygens.facetedsearch.model.parameters.IndexDescription;
 
 import org.apache.solr.client.solrj.SolrQuery;
@@ -19,22 +15,6 @@ public interface SolrSearcher {
    * @throws SolrServerException when the {@code SolrServer} throws it.
    */
   QueryResponse search(SolrQuery query) throws SolrServerException;
-
-  /**
-   * @deprecated use getIndexDescription
-   * Get all the facet information of this core.
-   * @return 
-   */
-  @Deprecated
-  List<FacetDefinition> getFacetDefinitions();
-
-  /**
-   * @deprecated use getIndexDescription
-   * Get a map with the facet information of this core. The keys are the names of the facets.
-   * @return
-   */
-  @Deprecated
-  Map<String, FacetDefinition> getFacetDefinitionMap();
 
   /**
    * Get a description of the index, the searches are executed on.
