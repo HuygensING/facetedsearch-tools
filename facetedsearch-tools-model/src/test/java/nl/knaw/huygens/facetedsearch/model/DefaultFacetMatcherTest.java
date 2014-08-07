508ac7f366ebd5542fcac4923868278bdf82e382
Martijn Maas <martijn.maas@huygens.knaw.nl>
2014-08-07 14:39:54 +0200

    [#2629] Let the faceted search library return the right facet type

--- facetedsearch-tools-model/src/test/java/nl/knaw/huygens/facetedsearch/model/DefaultFacetMatcherTest.java
+++ facetedsearch-tools-model/src/test/java/nl/knaw/huygens/facetedsearch/model/DefaultFacetMatcherTest.java
@@ -18,7 +18,7 @@ public class DefaultFacetMatcherTest {
     List<FacetOption> options = Lists.newArrayList(option1, option2);
     String facetTitle = "title";
     String facetName = "name";
-    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, facetTitle, options);
+    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, facetTitle, options, FacetType.LIST);
 
     DefaultFacet facet = new DefaultFacet(facetName, facetTitle);
     facet.setOptions(options);
@@ -33,7 +33,7 @@ public class DefaultFacetMatcherTest {
 
     List<FacetOption> options = Lists.newArrayList(option1, option2);
     String facetTitle = "title";
-    DefaultFacetMatcher matcher = new DefaultFacetMatcher("expectedName", facetTitle, options);
+    DefaultFacetMatcher matcher = new DefaultFacetMatcher("expectedName", facetTitle, options, FacetType.LIST);
 
     DefaultFacet facet = new DefaultFacet("actualName", facetTitle);
     facet.setOptions(options);
@@ -48,7 +48,7 @@ public class DefaultFacetMatcherTest {
 
     List<FacetOption> options = Lists.newArrayList(option1, option2);
     String facetName = "name";
-    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, "exepectedTitle", options);
+    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, "exepectedTitle", options, FacetType.LIST);
 
     DefaultFacet facet = new DefaultFacet(facetName, "actualTitle");
     facet.setOptions(options);
@@ -64,7 +64,7 @@ public class DefaultFacetMatcherTest {
     List<FacetOption> options = Lists.newArrayList(option1, option2);
     String facetTitle = "title";
     String facetName = "name";
-    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, facetTitle, options);
+    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, facetTitle, options, FacetType.LIST);
 
     DefaultFacet facet = new DefaultFacet(facetName, facetTitle);
     facet.setOptions(Lists.newArrayList(option2, option1));
@@ -80,7 +80,7 @@ public class DefaultFacetMatcherTest {
     List<FacetOption> expectedOptions = Lists.newArrayList(option1, option2);
     String facetTitle = "title";
     String facetName = "name";
-    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, facetTitle, expectedOptions);
+    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, facetTitle, expectedOptions, FacetType.LIST);
 
     DefaultFacet facet = new DefaultFacet(facetName, facetTitle);
     FacetOption option3 = new FacetOption("option3", 600L);
@@ -97,7 +97,7 @@ public class DefaultFacetMatcherTest {
     List<FacetOption> expectedOptions = Lists.newArrayList(option1, option2);
     String facetTitle = "title";
     String facetName = "name";
-    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, facetTitle, expectedOptions);
+    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, facetTitle, expectedOptions, FacetType.LIST);
 
     DefaultFacet facet = new DefaultFacet(facetName, facetTitle);
     FacetOption option3 = new FacetOption("option3", 600L);
@@ -114,10 +114,26 @@ public class DefaultFacetMatcherTest {
     List<FacetOption> expectedOptions = Lists.newArrayList(option1, option2);
     String facetTitle = "title";
     String facetName = "name";
-    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, facetTitle, expectedOptions);
+    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, facetTitle, expectedOptions, FacetType.LIST);
 
     DefaultFacet facet = new DefaultFacet(facetName, facetTitle);
     facet.setOptions(Lists.newArrayList(option1));
+
+    assertFalse(matcher.matchesSafely(facet));
+  }
+
+  @Test
+  public void testMatchesSafelyDifferentType() {
+    FacetOption option1 = new FacetOption("option1", 100L);
+    FacetOption option2 = new FacetOption("option2", 20L);
+
+    List<FacetOption> options = Lists.newArrayList(option1, option2);
+    String facetTitle = "title";
+    String facetName = "name";
+    DefaultFacetMatcher matcher = new DefaultFacetMatcher(facetName, facetTitle, options, FacetType.BOOLEAN);
+
+    DefaultFacet facet = new DefaultFacet(facetName, facetTitle);
+    facet.setOptions(options);
 
     assertFalse(matcher.matchesSafely(facet));
   }
